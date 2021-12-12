c87e1f2db3e1fe83d16b4457845e4617d441106e
Grant Sherman <tnarg122@gmail.com>
2021-12-12 13:45:59 -0500

    day 2 part 2 solution

--- go/day_2/day_2.go
+++ go/day_2/day_2.go
@@ -1,1 +1,54 @@
 package day_2
+
+import (
+	"errors"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/Grantsuu/advent_of_code_2021/util"
+)
+
+func Day_2_solutions() error {
+	log.Println("Day 2 Solution")
+
+	input, err := util.ParseInputFile("day_2/day_2_input.txt")
+	if err != nil {
+		return err
+	}
+
+	res, err := part_1(input)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Part 1 Answer:", res)
+
+	return nil
+}
+
+func part_1(input []string) (int, error) {
+
+	horizontal := 0
+	depth := 0
+
+	for _, k := range input {
+		s := strings.Split(k, " ")
+		if s[0] == "forward" {
+			i, _ := strconv.Atoi(s[1])
+			horizontal += i
+		} else if s[0] == "down" {
+			i, _ := strconv.Atoi(s[1])
+			depth += i
+		} else if s[0] == "up" {
+			i, _ := strconv.Atoi(s[1])
+			depth -= i
+		} else {
+			return 0, errors.New("invalid instruction")
+		}
+	}
+
+	log.Println("horizontal", horizontal, "depth", depth)
+
+	return horizontal * depth, nil
+}
--- go/main.go
+++ go/main.go
@@ -4,13 +4,18 @@ import (
 	"log"
 
 	"github.com/Grantsuu/advent_of_code_2021/day_1"
+	"github.com/Grantsuu/advent_of_code_2021/day_2"
 )
 
 func main() {
 	log.Println("Advent of Code 2021")
 
 	err := day_1.Day_1_solutions()
+	if err != nil {
+		log.Println(err.Error())
+	}
 
+	err = day_2.Day_2_solutions()
 	if err != nil {
 		log.Println(err.Error())
 	}
