173fa18e395f628bf58956b1b0585fd1205d08c8
Grant Sherman <tnarg122@gmail.com>
2022-01-04 00:25:36 -0500

    day 3 part 2 solution

--- go/day_3/day_3.go
+++ go/day_3/day_3.go
@@ -69,7 +69,36 @@ func part_1(input []string) (int, error) {
 }
 
 func part_2(input []string) (int, error) {
-	return 0, nil
+
+	o2_value := 0
+	co2_value := 0
+
+	o2_values := input
+	co2_values := input
+
+	for i := range o2_values[0] {
+		o2_values, err := create_bit_slice(o2_values, i, true)
+		if err != nil {
+			return 0, err
+		}
+		if len(o2_values) == 1 {
+			o2_value, _ = convert_binary_string_to_decimal(o2_values[0])
+			break
+		}
+	}
+
+	for i := range co2_values[0] {
+		co2_values, err := create_bit_slice(co2_values, i, false)
+		if err != nil {
+			return 0, err
+		}
+		if len(co2_values) == 1 {
+			co2_value, _ = convert_binary_string_to_decimal(co2_values[0])
+			break
+		}
+	}
+
+	return o2_value * co2_value, nil
 }
 
 func convert_binary_string_to_decimal(s string) (int, error) {
@@ -84,4 +113,56 @@ func convert_binary_string_to_decimal(s string) (int, error) {
 	}
 
 	return res, nil
+}
+
+// create slice containing most or least common values of bits at position n
+// s slice of values, n bit position, b most common or least
+func create_bit_slice(s []string, n int, b bool) ([]string, error) {
+	var ret []string
+
+	x := 0
+
+	// find most common value at bit n
+	for _, k := range s {
+		y, err := strconv.Atoi(string(k[n]))
+		if err != nil {
+			return nil, err
+		}
+		x += y
+	}
+
+	// determine which values to keep based on o2 or co2 criteria
+	if b {
+		if x >= int(math.Ceil(float64(float64(len(s))/2))) {
+			x = 1
+		} else {
+			x = 0
+		}
+	} else {
+		if x >= int(math.Ceil(float64(float64(len(s))/2))) {
+			x = 1
+		} else {
+			x = 0
+		}
+	}
+
+	// append over determined criteria
+	for _, k := range s {
+		y, err := strconv.Atoi(string(k[n]))
+		if err != nil {
+			return nil, err
+		}
+
+		if b {
+			if y == x {
+				ret = append(ret, k)
+			}
+		} else {
+			if y != x {
+				ret = append(ret, k)
+			}
+		}
+	}
+
+	return ret, nil
 }
